f45eeb24a1eca636cfa85cff94e5edc7f8c02a96
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-09-01 21:13:08 -0300

    feat: add structure app, and Fluent Validation

---
+++ src/CustomersCqrs.Application/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using CustomersCqrs.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CustomersCqrs.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Customer request)
+        {
+            var customer = new Customer(request.UserName, request.FirstName, request.LastName, request.Email, request.Cpf, request.Rg, request.Phone);
+
+            return Ok(customer);
+        }
+    }
+}
