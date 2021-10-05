929a50022ff624de942244bb0b4f99463cf30afe
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-10-05 20:58:03 -0300

    feat: update files

---
+++ src/Ecommerce.Domain/Validations/ClientValidator.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Domain.Entities;
+using FluentValidation;
+
+namespace Ecommerce.Domain.Validations
+{
+    public class ClientValidator : AbstractValidator<Client>
+    {
+        public ClientValidator()
+        {
+            RuleFor(x => x.FirstName)
+                        .NotNull()
+                        .NotEmpty()
+                        .WithMessage("FirstName is required!");
+
+            RuleFor(x => x.LastName)
+                        .NotNull()
+                        .NotEmpty()
+                        .WithMessage("LastName is required!");
+
+            RuleFor(x => x.Email)
+                        .NotNull()
+                        .NotEmpty()
+                        .EmailAddress();
+        }
+    }
+}
